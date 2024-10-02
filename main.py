2b90f05b4d93f3204b3e41120a0c784bd143f60e
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-02 10:44:10 +0300

    iterative and recursive functions

--- main.py
+++ main.py
@@ -1,67 +1,32 @@
-def my_function(name, last_name = ""):
-    print("Hello " + name + " " + last_name + " from my_function()")
+import time
 
-# positional parameters
-my_function("caner", "taşkın")
+# iterative function
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
-# keyword parameters
-my_function(last_name="taşkın", name="caner")
+five_fact = factorial(5)
 
-# default values
-my_function("caner")
+def factorial_recursive(n):
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n - 1)
 
-# non-default parameters must be set, otherwise we get an error
-# my_function()
-# my_function(last_name="taşkın")
+five_fact_recursive = factorial_recursive(5)
+print(five_fact_recursive)
 
-def foo(name = "x", last_name = "y"):
-    print("Hello " + name + " " + last_name + " from foo()")
+# be careful with max recursion depth in Python, which is 1000 by default
+# for n in [5, 50, 250, 1000, 5000]:
+for n in [5, 50, 250, 500, 750]:
+    print ("calculating ", n, "!")
+    start_i = time.time()
+    result_i = factorial(n)
+    end_i = time.time()
+    print ("iterative result: ", result_i, " time: ", end_i - start_i)
 
-foo()
-foo(last_name="fsdfs")
-
-def sum(a, b = 0, c = 0):
-    return a + b + c
-
-s = sum(3, 5)
-print("sum(3, 5) = ", s)
-
-s2 = sum(3)
-print("sum(3) = ", s2)
-
-s3 = sum(3, 5, 7)
-print("sum(3, 5, 7) = ", s3)
-
-# arbitrary arguments
-def sum_all(*all):
-    total = 0
-    for i in all:
-        total += i
-    return total
-
-s4 = sum_all()
-print ("sum_all() = ", s4)
-
-s5 = sum_all(4, 2, 6, 3, 67)
-print ("sum_all(4, 2, 6, 3, 67) = ", s5)
-
-def bar(x, y, *rest):
-    pass
-
-bar(10, 5, 35, 35, "fadfas", 3.24)
-
-
-# function parameters are local variables
-def check_age(age):
-    # avoid using global variables
-    # global age
-    age = 15
-    if age >= 18:
-        print ("You are an adult because your age is ", age)
-    else:
-        print ("You are not yet an adult because your age is ", age)
-
-age = 29
-print ("age before function call ", age)
-check_age(age)
-print ("age after function call ", age)
+    start_r = time.time()
+    result_r = factorial_recursive(n)
+    end_r = time.time()
+    print ("recursive result: ", result_r, " time: ", end_r - start_r)
