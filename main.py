493423b1f15cdba4af2addeae8472fe8c49f64d3
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-02 09:29:09 +0300

    default values

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def my_function(name, last_name):
+def my_function(name, last_name = ""):
     print("Hello " + name + " " + last_name + " from my_function()")
 
+# positional parameters
 my_function("caner", "taşkın")
 
+# keyword parameters
 my_function(last_name="taşkın", name="caner")
+
+# default values
+my_function("caner")
+
+# non-default parameters must be set, otherwise we get an error
+# my_function()
+# my_function(last_name="taşkın")
+
+def foo(name = "x", last_name = "y"):
+    print("Hello " + name + " " + last_name + " from foo()")
+
+foo()
+foo(last_name="fsdfs")
