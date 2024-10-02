4df70f84fbabf0f3b7dd6f41f8fb4f85d7fa9e96
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-02 09:44:53 +0300

    arbitrary parameters

--- main.py
+++ main.py
@@ -19,3 +19,33 @@ def foo(name = "x", last_name = "y"):
 
 foo()
 foo(last_name="fsdfs")
+
+def sum(a, b = 0, c = 0):
+    return a + b + c
+
+s = sum(3, 5)
+print("sum(3, 5) = ", s)
+
+s2 = sum(3)
+print("sum(3) = ", s2)
+
+s3 = sum(3, 5, 7)
+print("sum(3, 5, 7) = ", s3)
+
+# arbitrary arguments
+def sum_all(*all):
+    total = 0
+    for i in all:
+        total += i
+    return total
+
+s4 = sum_all()
+print ("sum_all() = ", s4)
+
+s5 = sum_all(4, 2, 6, 3, 67)
+print ("sum_all(4, 2, 6, 3, 67) = ", s5)
+
+def bar(x, y, *rest):
+    pass
+
+bar(10, 5, 35, 35, "fadfas", 3.24)
