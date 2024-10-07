d275460e927a7a9cc6eee62c8df171bf748ee166
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-07 10:23:31 +0300

    fibonacci_recursive

--- main.py
+++ main.py
@@ -17,3 +17,24 @@ print(fib5)
 
 fib2 = fibonacci_iterative(2)
 print(fib2)
+
+def fibonacci_recursive(n):
+    if n == 1 or n == 2:
+        return 1
+    fib1 = fibonacci_recursive(n - 1)
+    fib2 = fibonacci_recursive(n - 2)
+    return fib1 + fib2
+
+fib5r = fibonacci_recursive(5)
+print(fib5r)
+
+n = 10
+start_i = time.time()
+fib5 = fibonacci_iterative(5)
+end_i = time.time()
+print("fibonacci_iterative took ", end_i - start_i)
+
+start_r = time.time()
+fib5r = fibonacci_recursive(5)
+end_r = time.time()
+print("fibonacci_recursiv took ", end_r - start_r)
