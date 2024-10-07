2055a2a1f7028c26061f632512d66d389ccbb1a8
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-07 10:46:51 +0300

    fibonacci_memoized

--- main.py
+++ main.py
@@ -28,13 +28,30 @@ def fibonacci_recursive(n):
 fib5r = fibonacci_recursive(5)
 print(fib5r)
 
-n = 10
+# cache: dictionary n -> F(n)
+def fibonacci_memoized(n, cache):
+    if n == 1 or n == 2:
+        return 1
+    if n in cache:
+        return cache[n]
+    fib1 = fibonacci_memoized(n - 1, cache)
+    fib2 = fibonacci_memoized(n - 2, cache)
+    cache[n] = fib1 + fib2
+    return fib1 + fib2    
+
+n = 40
 start_i = time.time()
-fib5 = fibonacci_iterative(5)
+fib5 = fibonacci_iterative(n)
 end_i = time.time()
 print("fibonacci_iterative took ", end_i - start_i)
 
+cache = {}
+start_m = time.time()
+fib5m = fibonacci_memoized(n, cache)
+end_m = time.time()
+print("fibonacci_memoized took ", end_m - start_m)
+
 start_r = time.time()
-fib5r = fibonacci_recursive(5)
+fib5r = fibonacci_recursive(n)
 end_r = time.time()
-print("fibonacci_recursiv took ", end_r - start_r)
+print("fibonacci_recursive took ", end_r - start_r)
