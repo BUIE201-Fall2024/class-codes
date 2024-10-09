464c7cb85cae7f5ce742356e3285172df2a04c58
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-09 10:38:32 +0300

    set_date function and data encapsulation

--- main.py
+++ main.py
@@ -19,4 +19,45 @@ std2.Name = "tamer"
 print(std2.ID)
 print(std2.Name)
 
+def print_date(date):
+    print("The date printed using print_date is {}/{}/{}".format(date.Day, date.Month, date.Year))
 
+class Date:
+    def __init__(self) -> None:
+        self.Year = 1970
+        self.Month = 1
+        self.Day = 1
+    
+    def print(self):
+        print("The date is {}/{}/{}".format(self.Day, self.Month, self.Year))
+
+    def set_date(self, Year, Month, Day):
+        if Month < 1 or Month > 12:
+            print("Invalid month")
+        elif Day < 1 or Day > 31:
+            print("Invalid day")
+        # additional validation rules
+        else:
+            self.Day = Day
+            self.Month = Month
+            self.Year = Year
+
+today = Date()
+today.Year = 2024
+today.Month = 10
+today.Day = 9
+
+today.print()
+# global functions can still be used, but may violate 
+# data encapsulation principle in object-oriented design
+print_date(today)
+
+tomorrow = Date()
+# tomorrow.Year = 2024
+# tomorrow.Month = 100
+# tomorrow.Day = 10
+tomorrow.set_date(2024, 10, 10)
+tomorrow.print()
+
+tomorrow.set_date(2024, -10, 10)
+tomorrow.print()
