75f21cd16c55699f16728efd3454a09aefeef69c
Z. Caner Taşkın <caner.taskin@icrontech.com>
2024-10-14 09:43:44 +0300

    id function and everything is an object

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-class Student:
-    def __init__(self) -> None:
-        self.ID = 0
-        self.Name = None
-
-std1 = Student()
-std1.ID = 34242
-std1.Name = "caner"
-print(std1.ID)
-print(std1.Name)
-
-std2 = Student()
-std2.ID = 42453
-std2.Name = "tamer"
-
-# don't use attributes that are not defined in the class
-# std2.Surname = "unal"
-
-print(std2.ID)
-print(std2.Name)
-
-def print_date(date):
-    print("The date printed using print_date is {}/{}/{}".format(date.Day, date.Month, date.Year))
-
 class Date:
     def __init__(self, Year = 1970, Month = 1, Day = 1) -> None:
         self.Year = 1970
         self.Month = 1
         self.Day = 1
         self.set_date(Year, Month, Day)
-        # self.Year = Year
-        # self.Month = Month
-        # self.Day = Day
     
     def print(self):
         print("The date is {}/{}/{}".format(self.Day, self.Month, self.Year))
@@ -46,29 +19,34 @@ class Date:
             self.Month = Month
             self.Year = Year
 
-today = Date()
-today.Year = 2024
-today.Month = 10
-today.Day = 9
+today = Date(2024, 10, 14)
+print("today id: ", id(today))
 
-today.print()
-# global functions can still be used, but may violate 
-# data encapsulation principle in object-oriented design
-print_date(today)
+tomorrow = Date(2024, 10, 15)
+print("tomorrow id: ", id(tomorrow))
 
-tomorrow = Date()
-# tomorrow.Year = 2024
-# tomorrow.Month = 100
-# tomorrow.Day = 10
-tomorrow.set_date(2024, 10, 10)
-tomorrow.print()
+# everything is an object in Python
+i = 4
+# i = int(4)
+print("type(i): ", type(i))
+print("id(i): ", id(i))
 
-tomorrow.set_date(2024, -10, 10)
-tomorrow.print()
+f = 3.14
+# f = float(3.14)
+print("type(f): ", type(f))
+print("id(f): ", id(f))
 
-next_wednesday = Date(2024, 10, 16)
-next_wednesday.print()
-# Date.print(next_wednesday)
+s = "hello, IE 201"
+# s = str("hello, IE 201")
+print("type(s): ", type(s))
+print("id(s): ", id(s))
 
-next_tuesday = Date(-3424, 432, 324)
-next_tuesday.print()
+i2f = float(i)
+print("type(i2f): ", type(i2f))
+print("id(i2f): ", id(i2f))
+
+# float has a number of functions
+i2f.is_integer()
+
+print(id(s))
+print(id(s.capitalize()))
